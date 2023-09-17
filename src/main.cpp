a6afba13033fd5d64dd9dbf851e37387a0f792b0
MrHardWay <gera_agr@mail.ru>
2023-09-17 20:32:44 +0300

    commit 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	std::cout << "!!!Hello World!!!" << std::endl; // prints !!!Hello World!!!
 	return 0;
 }
